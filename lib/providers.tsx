ea68513f264279672db86f2dc99558c46dc4c468
zainabfatima097 <zainabfatima.zaman@gmail.com>
2026-05-12 00:38:30 +0530

    theme-provider

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -1,14 +1,21 @@
 "use client";
-
 import { SessionProvider } from "next-auth/react";
 import { HistoryProvider } from "./contexts/HistoryContext";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <HistoryProvider>{children}</HistoryProvider>
-      <Toaster />
+      <ThemeProvider 
+        attribute="class" 
+        defaultTheme="system" 
+        enableSystem
+        disableTransitionOnChange
+      >
+        <HistoryProvider>{children}</HistoryProvider>
+        <Toaster />
+      </ThemeProvider>
     </SessionProvider>
   );
 }
