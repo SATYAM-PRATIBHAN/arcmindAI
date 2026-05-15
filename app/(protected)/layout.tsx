3fe2d0279f564f397aa8a1f7aa49cbbb8037674f
zainabfatima097 <zainabfatima.zaman@gmail.com>
2026-05-15 09:22:56 +0530

    Add theme toggle button to navbar with sun/moon icons

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Sans, Geist_Mono } from "next/font/google";
-import { Providers } from "../../lib/providers";
+import { Providers } from "../lib/providers";
 import "./globals.css";
 import "./prism.css";
 
