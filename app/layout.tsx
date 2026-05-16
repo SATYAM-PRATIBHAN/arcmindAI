1a79a0924f1bdf3152ee384fc0248cd6eccc4993
zainabfatima097 <zainabfatima.zaman@gmail.com>
2026-05-16 20:37:41 +0530

    fixed hydration issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import { DM_Sans, Geist_Mono } from "next/font/google";
-import { Providers } from "../lib/providers";
+import { Providers } from "@/lib/providers";
 import "./globals.css";
 import "./prism.css";
-
 
 const dmSans = DM_Sans({
   variable: "--font-sans",
@@ -87,7 +86,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${dmSans.variable} ${geistMono.variable} antialiased`}>
         <Providers>{children}</Providers>
       </body>
